dbb1b5915f003ad2c6d99e605ebeeebe6af0d819
CLEO SILVA <csilva@mundolivre.digital>
2023-02-22 12:01:27 -0300

    feat: criando classes Curso e Mentoria | aprendendo sobre encapsulamento

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Fundamentos de Java");
+        curso1.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+
+        mentoria.setTitulo("Programador Java");
+        mentoria.setDescricao("Preparando para o mercado de trabalho");
+        mentoria.setData(LocalDate.of(2023, 3, 5));
+
+        System.out.println(curso1.toString());
+        System.out.println(mentoria.toString());
     }
 }
