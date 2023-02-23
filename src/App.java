60883bdd815a4ab10967cc3caf025d18cd8368ca
CLEO SILVA <csilva@mundolivre.digital>
2023-02-23 11:55:16 -0300

    Finalizado

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 public class App {
@@ -11,13 +13,46 @@ public class App {
         curso1.setDescricao("Fundamentos de Java");
         curso1.setCargaHoraria(8);
 
+        Curso curso2 = new Curso();
+
+        curso2.setTitulo("Curso Javascript");
+        curso2.setDescricao("Javascript Fundamentos");
+        curso2.setCargaHoraria(4);
+
+
         Mentoria mentoria = new Mentoria();
 
-        mentoria.setTitulo("Programador Java");
+        mentoria.setTitulo("Mentoria Programador Java");
         mentoria.setDescricao("Preparando para o mercado de trabalho");
         mentoria.setData(LocalDate.of(2023, 3, 5));
 
-        System.out.println(curso1.toString());
-        System.out.println(mentoria.toString());
+        // System.out.println(curso1.toString());
+        // System.out.println(mentoria.toString());
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Java Developer");
+        bootcamp.setDescricao("Descrição do bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devCleo = new Dev();
+        devCleo.setNome("Cleo");
+        devCleo.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Cleo: " + devCleo.getConteudosInscritos());
+        devCleo.progredir();
+        System.out.println("Conteúdos concluídos Cleo: " + devCleo.getConteudoConcluidos());
+
+
+        Dev devJose = new Dev();
+        devJose.setNome("José Lucas");
+        devJose.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos José: " + devJose.getConteudosInscritos());
+        devJose.progredir();
+        System.out.println("Conteúdos concluídos José: " + devJose.getConteudoConcluidos());
+
+
+
+
     }
 }
